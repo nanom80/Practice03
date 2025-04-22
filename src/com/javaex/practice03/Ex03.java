3ca78dd08ae5b1f2c4f5517f597a044cfabb7de8
nanom80 <nanomegi@gmail.com>
2025-04-22 17:30:43 +0900

    2025-04-22 17:30

--- src/com/javaex/practice03/Ex03.java
+++ src/com/javaex/practice03/Ex03.java
@@ -1,12 +1,12 @@
 package com.javaex.practice03;
 
-public class Ex02 {
+public class Ex03 {
 	
 	public static void main(String[] args) {
 		
 		int x, y;
 		for(x=0; x<4; x++) {
-			for(y=0; y<2; y++) {
+			for(y=2; y>=0; y--) {
 				System.out.println("*");
 			}
 			System.out.println("");
